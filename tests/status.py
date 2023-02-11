9e8365cfcdcd5a1707db74dfaaa0365252097c57
jkrperson <josekristianresabal@gmail.com>
2023-02-12 01:30:14 +0800

    test: Add httpretty to timeline testing

--- tests/status.py
+++ tests/status.py
@@ -10,10 +10,30 @@ import tweepy
 from base64 import b64encode
 import requests
 
+import httpretty
+
+
 def test_twitter_timeline():
     """
     We can mock the calls using https://github.com/gabrielfalcao/HTTPretty
     """
+    httpretty.enable(verbose=True, allow_net_connect=False)
+    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/me",
+                           body="""{'data':{'id': '123123123','name': 'john','username': 'johnyhon'}}""",
+                           status=200)
+
+    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/123123123/mentions",
+                           body="""{'data':[
+                            {
+                                'id': '111111',
+                                'edit_history_tweet_ids': [
+                                    '111111'
+                                ],
+                                'text': '@johnyhon hello'
+                                
+                                
+                                """)
+
     pass
 
 def test_mastodon_timeline():
