2c44fae6adc49f429dfa6f8db74fb53c68291109
jkrperson <josekristianresabal@gmail.com>
2023-02-11 23:42:04 +0800

    test: Add test template for timeline generation

---
+++ tests/status.py
@@ -0,0 +1,23 @@
+from minsocial.status import (
+    Timeline, Tweet, Toot
+)
+
+import pytest
+import json
+
+import tweepy
+
+from base64 import b64encode
+import requests
+
+def test_twitter_timeline():
+    """
+    We can mock the calls using https://github.com/gabrielfalcao/HTTPretty
+    """
+    pass
+
+def test_mastodon_timeline():
+    pass
+
+if __name__ == "__main__":
+    test_twitter_timeline()
