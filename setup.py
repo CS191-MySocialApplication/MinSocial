4ccd79aefec875c10c795e150835ead3a525aa81
Jose Kristian Resabal <jkresabal@up.edu.ph>
2023-02-12 12:50:05 +0800

    test: Creat tests for timeline with httpretty

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'flask',
         'tweepy',
-        'Mastodon.py'
+        'Mastodon.py',
+        'httpretty'
     ],
 )
--- tests/status.py
+++ tests/status.py
@@ -1,8 +1,7 @@
 from minsocial.status import (
-    Timeline, Tweet, Toot
+    Timeline, Tweet, Toot, Status
 )
 
-import pytest
 import json
 
 import tweepy
@@ -17,27 +16,85 @@ def test_twitter_timeline():
     """
     We can mock the calls using https://github.com/gabrielfalcao/HTTPretty
     """
+
+    with open("tests/simulated_responses/twt_me.json", "r", encoding="utf-8") as me_file:
+        me_body = me_file.read()
+
+    with open("tests/simulated_responses/twt_home.json", "r", encoding="utf-8") as test_file:
+        test_body = test_file.read()
+
     httpretty.enable(verbose=True, allow_net_connect=False)
     httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/me",
-                           body="""{'data':{'id': '123123123','name': 'john','username': 'johnyhon'}}""",
-                           status=200)
+                           body=me_body,
+                           status=200,
+                           content_type="text/json")
 
-    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/123123123/mentions",
-                           body="""{'data':[
-                            {
-                                'id': '111111',
-                                'edit_history_tweet_ids': [
-                                    '111111'
-                                ],
-                                'text': '@johnyhon hello'
-                                
-                                
-                                """)
+    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/994289933354332160/mentions",
+                            body=test_body,
+                            status=200)
+    
+    timeline = Timeline(twtAccessKey="sdfsdf")
 
-    pass
+    assert type(timeline[0]) is Tweet
+
 
 def test_mastodon_timeline():
-    pass
+
+    with open("tests/simulated_responses/mstdn_home.json", "r", encoding="utf-8") as test_file:
+        test_body = test_file.read()
+
+    with open("tests/simulated_responses/mstdn_instance.json", "r", encoding="utf-8") as instance_file:
+        instance_body = instance_file.read()
+
+    httpretty.enable(verbose=True, allow_net_connect=False)
+    httpretty.register_uri(httpretty.GET, "https://social.up.edu.ph/api/v1/notifications",
+                            body=test_body,
+                            status=200)
+    
+    httpretty.register_uri(httpretty.GET, "https://social.up.edu.ph/api/v1/instance/",
+                           body=instance_body,
+                           status=200)
+    
+    timeline = Timeline(mstdnAccessKey="asdasd")
+
+    assert(type(timeline[0]) is Toot)
+
+
+def test_sorted_timeline():
+
+    with open("tests/simulated_responses/twt_me.json", "r", encoding="utf-8") as me_file:
+        me_body = me_file.read()
+
+    with open("tests/simulated_responses/twt_home.json", "r", encoding="utf-8") as test_file:
+        twt_test_body = test_file.read()
+
+    with open("tests/simulated_responses/mstdn_home.json", "r", encoding="utf-8") as test_file:
+        mstdn_test_body = test_file.read()
+
+    with open("tests/simulated_responses/mstdn_instance.json", "r", encoding="utf-8") as instance_file:
+        instance_body = instance_file.read()
+
+    httpretty.enable(verbose=True, allow_net_connect=False)
+    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/me",
+                           body=me_body,
+                           status=200,
+                           content_type="text/json")
+
+    httpretty.register_uri(httpretty.GET, "https://api.twitter.com/2/users/994289933354332160/mentions",
+                            body=twt_test_body,
+                            status=200)
+    
+    httpretty.register_uri(httpretty.GET, "https://social.up.edu.ph/api/v1/notifications",
+                            body=mstdn_test_body,
+                            status=200)
+    
+    httpretty.register_uri(httpretty.GET, "https://social.up.edu.ph/api/v1/instance/",
+                           body=instance_body,
+                           status=200)
+    
+    timeline = Timeline(twtAccessKey="asd", mstdnAccessKey="asdasd")
+
+    assert sorted(timeline.statusList, key=lambda x: x.createdTime, reverse=True) == timeline.statusList
 
 if __name__ == "__main__":
-    test_twitter_timeline()
+    test_sorted_timeline()
