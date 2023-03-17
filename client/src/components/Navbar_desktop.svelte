7630b6fc785757236e43dc9e71fec10903fcbc16
Jeanne <jeanneclarissetoledo@gmail.com>
2023-03-17 18:08:00 +0800

    feat: Create navbar for desktop

---
+++ client/src/components/Navbar_desktop.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<main>
+    <nav class="nav-bar-desktop">
+        <!--No icons yet. li placed in the meantime-->
+        <ul>
+            <li><a href="login.html">DMs</a></li>
+            <li><a href="login.html">Log out</a></li>
+        </ul>
+    </nav>
+</main>
+
+<style>
+    main {
+        margin: 0;
+        background-color: #50c0cb;
+        color: white;
+    }
+    @media screen and (max-width:479px) {
+        main {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width:480px) and (max-width: 899px) {
+        main {
+            width: 24%;
+            margin-right: 20px;
+        }
+    }
+
+    @media screen and (min-width:900px) {
+        main {
+            width: 14%;
+            margin-right: 20px;
+        }
+    }
+
+</style>
--- client/src/home/App.svelte
+++ client/src/home/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from "../components/Header.svelte";
 	import Postform from "../components/postform.svelte";
+	import Navbar_desktop from "../components/Navbar_desktop.svelte"
 
 	async function getHomeContent(){
 
@@ -18,31 +19,34 @@
 	let auth_promise = getHomeContent();
 
 </script>
+<div class="desktop-format">
+	<Navbar_desktop />
+	<div class="content">
+		<Header />
+		<main>
 
-	<Header />
-	<main>
+			<Postform/>
 
-		<Postform/>
+			{#await auth_promise}
+				<p>waiting...</p>
+			{:then timeline}
+				{#each timeline as status}
 
-		{#await auth_promise}
-			<p>waiting...</p>
-		{:then timeline}
-			{#each timeline as status}
+					<div class="post">
+						<span id="source" class="impt-details">{status["source"]} |</span> 
+						<span id="username" class="impt-details">{status["author"]["username"]}</span><br/>
+						<span id="datetime">{status["createdTime"]}</span><br/>
+						<p>{status["content"]}</p><br/><br/>
+					</div>
 
-				<div class="post">
-					<span id="source" class="impt-details">{status["source"]} |</span> 
-					<span id="username" class="impt-details">{status["author"]["username"]}</span><br/>
-					<span id="datetime">{status["createdTime"]}</span><br/>
-					<p>{status["content"]}</p><br/><br/>
-				</div>
+				{/each}
+			{:catch error}
+				<p style="color: red">{error.message}</p>
+			{/await}
 
-			{/each}
-		{:catch error}
-			<p style="color: red">{error.message}</p>
-		{/await}
-
-	</main>
-
+		</main>
+	</div>
+</div>
 <style>
 
 	main {
@@ -54,6 +58,21 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
+	}
+
+	@media (max-width: 479px) {
+		.desktop-format {  
+            display: flex;
+            flex-direction: column;
+            align-items: stretch;
+        }
+	}
+
+	@media (min-width: 480px) {
+		.desktop-format {
+            display: flex;
+            flex-direction: row;
+        }
 	}
 
 </style>
