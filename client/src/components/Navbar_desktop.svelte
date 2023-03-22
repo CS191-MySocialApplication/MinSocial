3c3377ee9c7596cab532b47706ab5ffc945f4aa9
jaspascuals <jasminvictoriapascual@gmail.com>
2023-03-22 10:26:58 +0800

    feat: add icons to navbar

--- client/src/components/Navbar_desktop.svelte
+++ client/src/components/Navbar_desktop.svelte
@@ -1,13 +1,30 @@
 <script>
+    
 </script>
 
 <main>
     <nav class="nav-bar-desktop">
-        <!--No icons yet. li placed in the meantime-->
-        <ul>
-            <li><a href="login.html">DMs</a></li>
-            <li><a href="login.html">Log out</a></li>
-        </ul>
+        
+        <a class="mentions" href="login.html">
+            <div class="clicked">
+                <img src="/public/clicked_mentions.png" alt="clicked mentions"/>
+                <img src="/public/hover_clicked_mentions.png" class="hover_img" alt="hover clicked mentions"/>
+            </div>
+            <div class="unclicked">
+                <img src="/public/unclicked_mentions.png" alt="unclicked mentions"/>
+                <img src="/public/hover_unclicked_mentions.png" class="hover_img" alt="hover unclicked mentions"/>
+            </div>
+        </a>
+        <a class="dm" href="login.html">
+            <img src="/public/clicked_dm.png" alt="clicked dm"/>
+            <img src="/public/hover_clicked_dm.png" class="hover_img" alt="hover clicked dm"/>
+        </a>
+
+        <a class="settings" href="login.html">
+            <img src="/public/clicked_settings.png" alt="clicked settings"/>
+            <img src="/public/hover_clicked_settings.png" class="hover_img" alt="hover clicked settings"/>
+        </a>
+            
     </nav>
 </main>
 
@@ -36,5 +53,72 @@
             margin-right: 20px;
         }
     }
+    img{
+        width: 40px;
+        height: 40px;
+    }
 
+    .mentions{
+        margin-left: 50px;
+        margin-top: 30px;
+        position: absolute;
+        display: flex;
+    }
+
+    .clicked{
+        position: absolute;
+        display: flex;
+    }
+    
+    .dm{
+        border:none;
+        background-color: inherit;
+        margin-left: 50px;
+        margin-top: 100px;
+        position: absolute;
+        display: flex;
+    }
+
+    .settings{
+        border:none;
+        background-color: inherit;
+        margin-left: 50px;
+        margin-top: 170px;
+        position: absolute;
+        display: flex;
+    }
+    
+    .hover_img{
+        display: none;
+        position: absolute;    
+    }
+
+    .unclicked{
+        display: none;
+        position: absolute; 
+    }
+
+    .mentions:hover .hover_img{
+        display: inline;
+        opacity:0.5;
+        transition: 0.25s ease;
+    }
+    
+    .dm:hover .hover_img{
+        display: flex;
+        opacity:0.5;
+        transition: 0.25s ease;
+    }
+    
+    .settings:hover .hover_img{
+        display: flex;
+        opacity:0.5;
+        transition: 0.25s ease;
+    }
+
+    
 </style>
+
+
+
+
