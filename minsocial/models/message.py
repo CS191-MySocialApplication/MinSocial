0757a70e2c954933e63a9d1a20dd4d8a742b15f3
jkrperson <josekristianresabal@gmail.com>
2023-04-10 22:28:00 +0800

    refactor: Create data model for message

---
+++ minsocial/models/message.py
@@ -0,0 +1,42 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Message(metaclass=ABCMeta):
+    # TODO: Group conversation handling 
+    def __init__(self, messageID, content, author, conversationID, createdTime):
+        
+        self.messageID = messageID
+        self.content = content
+        self.author = author
+        self.conversationID = conversationID
+        self.createdTime = createdTime
+
+    def asdict(self):
+        return {
+            'source': self.source,
+            'messageID': self.messageID,
+            'content': self.content,
+            'author': self.author,
+            'conversationID': self.conversationID,
+            'createdTime': self.createdTime,
+        }
+
+    @property
+    @abstractmethod
+    def source(self):
+        pass
+
+
+class TwtMsg(Message):
+    source = "Twitter"
+
+    def __init__(self, message, user):
+
+        super().__init__(message["id"], message["text"], user, message["dm_conversation_id"], message["created_at"])
+
+
+class MstdnMsg(Message):
+    source = "Mastodon"
+
+    def __init__(self, conversationID, message):
+        super().__init__(message["id"], message["content"], message["account"], conversationID, message["created_at"])
