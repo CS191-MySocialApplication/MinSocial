48350a49c2044e156f78413d61d95f732c07eabc
jkrperson <josekristianresabal@gmail.com>
2023-04-10 22:25:25 +0800

    refactor: Create data model for Status

---
+++ minsocial/models/status.py
@@ -0,0 +1,58 @@
+from abc import ABCMeta, abstractmethod
+from datetime import datetime, timezone
+
+
+class Status(metaclass=ABCMeta):
+
+    def __init__(self, statusID, author, createdTime, content):
+
+        self.id = statusID
+        self.author = author
+        self.createdTime = createdTime
+        self.content = content
+
+    def asdict(self):
+        return {
+            'source': self.source,
+            'id': self.id,
+            'author': self.author,
+            'createdTime': self.createdTime.isoformat(),
+            'content': self.content
+        }
+
+    @property
+    @abstractmethod
+    def source(self):
+        pass
+
+
+class Tweet(Status):
+    source = "Twitter"
+    
+    def __init__(self, data: dict[str, any], author: dict[str, any]):
+
+        assert("id" in data)
+        assert("author_id" in data)
+        assert("created_at" in data)
+        assert("text" in data) 
+
+        createdTime = datetime.strptime(data["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        createdTime = createdTime.replace(tzinfo=timezone.utc)
+
+        super().__init__(data["id"], author, createdTime, data["text"])
+
+
+class Toot(Status):
+    source = "Mastodon"
+
+    def __init__(self, toot: dict[str, any]):
+        
+        assert("id" in toot)
+        assert("account" in toot)
+        assert("created_at" in toot)
+        assert("content" in toot)
+
+        createdTime = toot["created_at"]
+        createdTime = createdTime.replace(tzinfo=timezone.utc)
+
+        super().__init__(toot["id"], toot["account"], createdTime, toot["content"])
