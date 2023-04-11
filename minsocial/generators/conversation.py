9094c52124d9428e0708983b5486584aeacd5952
jkrperson <josekristianresabal@gmail.com>
2023-04-11 15:26:42 +0800

    doc: Add description and TODO for Mstdn conv.

--- minsocial/generators/conversation.py
+++ minsocial/generators/conversation.py
@@ -3,6 +3,9 @@ from mastodon import Mastodon
 
 def generate_mstdn_conversation(message_id, mstdn_access_key):
 
+    # Returns the parents of a message
+    # TODO: Make data model for messages (still Mastodon statuses)
+
     client = client = Mastodon(api_base_url="https://social.up.edu.ph", access_token=mstdn_access_key)
     response = client.status_context(message_id) 
 
