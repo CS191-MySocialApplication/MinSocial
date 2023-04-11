12c63e3bbfb5b354a0afc69a815fb8035f1e9251
Jose Kristian Resabal <jkresabal@up.edu.ph>
2023-04-11 17:28:50 +0800

    feat: Create parent blueprint for API endpoints

---
+++ minsocial/api/api.py
@@ -0,0 +1,12 @@
+from flask import Blueprint
+
+from minsocial.api.conversation import conversation_bp
+from minsocial.api.conversations_list import conversation_list_bp
+from minsocial.api.mentions_timeline import mentions_bp
+
+
+api_blueprint = Blueprint("api", __name__, url_prefix="/api")
+
+api_blueprint.register_blueprint(conversation_bp)
+api_blueprint.register_blueprint(conversation_list_bp)
+api_blueprint.register_blueprint(mentions_bp)
