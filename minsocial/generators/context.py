7e97e21aca2c7d08d02113771a5849a027e88716
jkrperson <josekristianresabal@gmail.com>
2023-04-11 13:42:02 +0800

    style: Change variable name for Mstdn access key

--- minsocial/generators/context.py
+++ minsocial/generators/context.py
@@ -3,9 +3,9 @@ from mastodon import Mastodon
 from minsocial.models.status import Toot
 
 
-def generate_mstdn_context(status_id, mstdn_access_id):
+def generate_mstdn_context(status_id, mstdn_access_key):
 
-    client = Mastodon(api_base_url="https://social.up.edu.ph", access_token=mstdn_access_id)
+    client = Mastodon(api_base_url="https://social.up.edu.ph", access_token=mstdn_access_key)
 
     context = client.status_context(status_id)
 
