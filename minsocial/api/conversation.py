f72e61066a26f7c160e4ebe1eeb07b0508ef25f2
Jose Kristian Resabal <jkresabal@up.edu.ph>
2023-04-11 16:06:12 +0800

    refactor: Separate Mstdn Conversation from route

---
+++ minsocial/api/conversation.py
@@ -0,0 +1,21 @@
+from flask import (
+    Blueprint, request
+)
+
+from minsocial.decorators import wrap_json, authenticate
+from minsocial.generators.conversation import generate_mstdn_conversation
+
+bp = Blueprint('conversation', __name__, url_prefix='/messages')
+
+@bp.route("/messages/mstdn/<conversation_id>")
+@authenticate
+@wrap_json
+def mstdnconversation(conversation_id):
+
+    # From a status id, return the conversation.
+
+    mstdnAccess = request.cookies.get("mstdnAccessToken")
+    
+    messageList = generate_mstdn_conversation(mstdnAccess, conversation_id)
+
+    return messageList
