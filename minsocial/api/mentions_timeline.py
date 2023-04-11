8796a251376225c6968e5a0eea8b1bd3f2f6aad7
jkrperson <josekristianresabal@gmail.com>
2023-04-11 15:15:18 +0800

    refactor: Separate mentions endpoint from routes

---
+++ minsocial/api/mentions_timeline.py
@@ -0,0 +1,23 @@
+from flask import (
+    Blueprint, request
+)
+
+from minsocial.decorators import wrap_json, authenticate
+from minsocial.generators.timeline import generate_mentions_timeline
+
+
+bp = Blueprint('mentions', __name__, url_prefix='/mentions')
+
+@bp.route("/home")
+@authenticate
+@wrap_json
+def home():
+
+    # Returns a list of Statuses that mentions the current user.
+
+    twtAccess = request.cookies.get("twtAccessToken")
+    mstdnAccess = request.cookies.get("mstdnAccessToken")
+
+    timeline = generate_mentions_timeline(twtAccessKey=twtAccess, mstdnAccessKey=mstdnAccess)
+
+    return timeline
