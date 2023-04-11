a0589e9719ed7bd5be695873585e827ec9048d58
jkrperson <josekristianresabal@gmail.com>
2023-04-11 13:41:15 +0800

    refactor: Create conversation list generator

---
+++ minsocial/generators/conversation_list.py
@@ -0,0 +1,36 @@
+from mastodon import Mastodon
+
+from minsocial.models.message import MstdnMsg
+
+def generate_conversation_list(twt_access_key=None, mstdn_access_key=None):
+    assert(twt_access_key is not None or mstdn_access_key is not None)
+
+    conversation_list = []
+
+    if twt_access_key:
+        twt_conversation_list = generate_twt_conversation_list(twt_access_key)
+        conversation_list.extend(twt_conversation_list)
+
+    if mstdn_access_key:
+        mstdn_conversation_list = generate_mstdn_conversation_list(mstdn_access_key)
+        conversation_list.extend(mstdn_conversation_list)
+
+    return conversation_list
+
+
+def generate_twt_conversation_list(twt_access_key): 
+
+    # ISSUE: Twitter API will eventually be unaccessible
+
+    pass
+
+
+def generate_mstdn_conversation_list(mstdn_access_key):
+
+    client = Mastodon(api_base_url="https://social.up.edu.ph", access_token=mstdn_access_key)
+
+    conversations_list = client.conversations()
+
+    for conversations in conversations_list:
+
+        yield MstdnMsg(conversations["id"], conversations["last_status"]).asdict()
