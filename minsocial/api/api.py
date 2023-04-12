fed1cfb50c45cd0fa86d74144a576442afcff1de
Jose Kristian Resabal <jkresabal@up.edu.ph>
2023-04-12 16:45:02 +0800

    refactor: Fix new api endpoint prefix

--- minsocial/api/api.py
+++ minsocial/api/api.py
@@ -5,7 +5,7 @@ from minsocial.api.conversations_list import conversation_list_bp
 from minsocial.api.mentions_timeline import mentions_bp
 
 
-api_blueprint = Blueprint("api", __name__, url_prefix="/api")
+api_blueprint = Blueprint("api", __name__, url_prefix="/api/v2")
 
 api_blueprint.register_blueprint(conversation_bp)
 api_blueprint.register_blueprint(conversation_list_bp)
