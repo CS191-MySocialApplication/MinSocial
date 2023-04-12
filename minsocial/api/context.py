6e9d12ff0719d9c539b215bfb40af85d37aa00fe
jkrperson <josekristianresabal@gmail.com>
2023-04-12 21:53:50 +0800

    refactor: Separate context endpoint from router

---
+++ minsocial/api/context.py
@@ -0,0 +1,19 @@
+from flask import (
+    Blueprint, request
+)
+
+from minsocial.decorators import wrap_json, authenticate
+from minsocial.generators.context import generate_mstdn_context
+
+context_bp = Blueprint('mentions', __name__, url_prefix='/home')
+
+
+@context_bp.route("/context/toot/<toot_id>")
+@authenticate
+@wrap_json
+def view_toot_context(toot_id):
+    mstdn_access_key = request.cookies.get("mstdnAccessToken")
+
+    context = generate_mstdn_context(toot_id, mstdn_access_key)
+
+    return context
