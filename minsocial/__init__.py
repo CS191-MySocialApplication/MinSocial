70a7ec9eb8701c2a61a3f7b7c49834a3acb42bef
jkrperson <josekristianresabal@gmail.com>
2023-05-11 11:44:50 +0800

    refact: Add .env support in server

--- minsocial/__init__.py
+++ minsocial/__init__.py
@@ -3,6 +3,9 @@ from minsocial.api import api
 from minsocial.userAuth import mstdnAuth, twtAuth
 from minsocial.pages import routes
 
+from dotenv import load_dotenv
+import os
+
 def create_app():
     app = Flask(__name__, static_url_path="/static", static_folder='../client/build/static')
 
@@ -10,5 +13,12 @@ def create_app():
     app.register_blueprint(mstdnAuth.bp)
     app.register_blueprint(api.api_blueprint)
     app.register_blueprint(routes.bp)
+
+    load_dotenv()
+
+    assert os.getenv("mastodon_api_base_url")
+    assert os.getenv("mastodon_client_id")
+    assert os.getenv("mastodon_client_secret")
+    assert os.getenv("server_host_url")
 
     return app
