d217cf4779314e075bd6d09b6467f9d45beb566c
jkrperson <josekristianresabal@gmail.com>
2023-05-11 11:45:54 +0800

    refact: Replace reading const to using .env file in context

--- minsocial/generators/context.py
+++ minsocial/generators/context.py
@@ -2,12 +2,12 @@ from mastodon import Mastodon
 
 from minsocial.models.status import Toot
 
-from minsocial import consts
+import os
 
 
 def generate_mstdn_context(status_id, mstdn_access_key):
 
-    client = Mastodon(api_base_url=consts.MSTDN_API_BASE_URL, access_token=mstdn_access_key)
+    client = Mastodon(api_base_url=os.getenv("mastodon_api_base_url"), access_token=mstdn_access_key)
 
     context = client.status_context(status_id)
 
