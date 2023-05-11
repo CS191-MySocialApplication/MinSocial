58adc46d3e4a5e126ce9a22fda22fc12dbeab7b5
jkrperson <josekristianresabal@gmail.com>
2023-05-11 11:46:28 +0800

    refact: Replace reading const to using .env file in status

--- minsocial/generators/status.py
+++ minsocial/generators/status.py
@@ -2,12 +2,12 @@ from mastodon import Mastodon
 
 from minsocial.models.status import Toot
 
-from minsocial import consts
+import os
 
 
 def generate_mstdn_status(status_id, mstdn_access_key):
 
-    client = Mastodon(api_base_url=consts.MSTDN_API_BASE_URL, access_token=mstdn_access_key)
+    client = Mastodon(api_base_url=os.getenv("mastodon_api_base_url"), access_token=mstdn_access_key)
 
     toot = client.status(status_id)
 
