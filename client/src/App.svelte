2cc9e6c1995823e417049149fb422054eb8325bd
jkrperson <josekristianresabal@gmail.com>
2023-05-24 20:02:43 +0800

    feat(SPA): convert main svelte file to handle SPA routes

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,37 +1,8 @@
 <script>
-	import "@fontsource/open-sans"
-
-	async function getMstdnAuthLink(){
-
-		let res = await fetch('/auth/mstdn');
-		let text = await res.text();
-
-		if (res.ok){
-			let auth_json = JSON.parse(text)
-			return auth_json["auth_url"];
-		} else{
-			throw new Error(text);
-		}
-	}
-
-	let mstdn_auth_promise = getMstdnAuthLink();
-
+	import Router from 'svelte-spa-router';
+	import routes from './routes';
 </script>
 
 <main>
-	{#await mstdn_auth_promise}
-		<p>waiting...</p>
-	{:then url}
-		<p>
-			<a href={url}>Log-in Using Mastodon</a>
-		</p>
-	{:catch error}
-		<p style="color: red">{error.message}</p>
-	{/await}
+	<Router {routes} />
 </main>
-
-<style>
-	a {
-		font-family: "Open Sans";
-	}
-</style>
